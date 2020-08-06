476c6d8f10f8be545066208e9863b9017ddfbf30
Marina <marina09oliveira@gmail.com>
2020-08-06 17:24:20 -0300

    terceiro dia NLW - coneção da api com a interface

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -1,6 +1,7 @@
 import React, { SelectHTMLAttributes } from 'react';
 
 import './styles.css';
+
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     name: string;
     label: string;
@@ -14,7 +15,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select defaultValue="" id={name} {...rest}>
+            <select value="" id={name} {...rest}>
                 <option value="" disabled hidden>
                     Selecione uma opção
                 </option>
